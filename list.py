0d69210460025142b910112cf444987b2b2ce5ea
Md Al Hossain Mukib <143283871+mdmokib@users.noreply.github.com>
2025-03-24 23:49:39 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California",
+    "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+    "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland"]
+
+states[2]="Aryzona"
+states.append("Dhaka")
+states.extend(["Mukib","Maruf"])
+
+print(states)
